bc4e08d3877fab22cbe3a2bf22a122aafd60acbc
basketterstan <131014195+basketterstan@users.noreply.github.com>
2026-05-25 11:15:12 +0200

    Update js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSa4HDuZn5K21Ty1E-uFBF9IXA_Pg8Sus",
+  authDomain: "treasureshirt-68b83.firebaseapp.com",
+  projectId: "treasureshirt-68b83",
+  storageBucket: "treasureshirt-68b83.firebasestorage.app",
+  messagingSenderId: "999550128041",
+  appId: "1:999550128041:web:d049592cc294c5a401beb8",
+  measurementId: "G-KNFS2XXS62"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
